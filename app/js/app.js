b4b38a2cd73814f06799fb70b713d819fc681808
Paolo Bernasconi <paolo.enrico.bernasconi@gmail.com>
2014-12-23 15:00:36 +0100

    mobile version

--- app/js/app.js
+++ app/js/app.js
@@ -10,52 +10,137 @@ angular.module('myApp', [
 ])
 
 		.run(function () {
-				particlesJS('particles-js', {
-						particles: {
-								color: '#fff',
-								shape: 'edge',
-								opacity: 1,
-								size: 2.5,
-								size_random: true,
-								nb: 150,
-								line_linked: {
-										enable_auto: true,
-										distance: 250,
+				/* DESKTOP */
+
+				function pJS_desktop() {
+						particlesJS('particles-js', {
+								particles: {
 										color: '#fff',
-										opacity: 0.5,
-										width: 1,
-										condensed_mode: {
-												enable: false,
-												rotateX: 600,
-												rotateY: 600
+										shape: 'circle',
+										opacity: 1,
+										size: 2.5,
+										size_random: true,
+										nb: 150,
+										line_linked: {
+												enable_auto: true,
+												distance: 250,
+												color: '#fff',
+												opacity: 0.5,
+												width: 1,
+												condensed_mode: {
+														enable: false,
+														rotateX: 600,
+														rotateY: 600
+												}
+										},
+										anim: {
+												enable: true,
+												speed: 3
 										}
 								},
-								anim: {
+								interactivity: {
 										enable: true,
-										speed: 2.5
-								}
-						},
-						interactivity: {
-								enable: true,
-								mouse: {
-										distance: 350
-								},
-								detect_on: 'canvas',
-								mode: 'grab',
-								line_linked: {
-										opacity: 0.5
-								},
-								events: {
-										onclick: {
-												push_particles: {
-														enable: false,
+										mouse: {
+												distance: 200
+										},
+										detect_on: 'canvas',
+										mode: 'grab',
+										line_linked: {
+												opacity: .5
+										},
+										events: {
+												onclick: {
+														enable: true,
+														mode: 'push',
 														nb: 4
 												}
 										}
+								},
+								retina_detect: true
+						});
+				}
+
+
+				/* MOBILE / TABLET */
+
+				function pJS_mobile() {
+						particlesJS('particles-js', {
+								particles: {
+										color: '#fff',
+										shape: 'circle',
+										opacity: 1,
+										size: 2.5,
+										size_random: true,
+										nb: 40,
+										line_linked: {
+												enable_auto: false,
+												distance: 250,
+												color: '#fff',
+												opacity: 0.5,
+												width: 1,
+												condensed_mode: {
+														enable: false,
+														rotateX: 600,
+														rotateY: 600
+												}
+										},
+										anim: {
+												enable: true,
+												speed: 3
+										}
+								},
+								interactivity: {
+										enable: false,
+										mouse: {
+												distance: 200
+										},
+										detect_on: 'canvas',
+										mode: 'grab',
+										line_linked: {
+												opacity: .5
+										},
+										events: {
+												onclick: {
+														enable: true,
+														mode: 'push',
+														nb: 4
+												}
+										}
+								},
+								retina_detect: true
+						});
+				}
+
+
+				/* LAUNCH */
+
+				if (window.innerWidth > 750) {
+						pJS_desktop();
+				} else {
+						pJS_mobile();
+				}
+
+				/* on resize */
+
+				window.addEventListener('resize', function () {
+						checkOnResize();
+				}, true);
+
+				function checkOnResize() {
+						if (window.innerWidth > 750) {
+								if (pJS.particles.nb != 150) {
+										console.log('desktop mode');
+										pJS.fn.vendors.destroy();
+										pJS_desktop();
 								}
-						},
-						retina_detect: true
-				});
+						} else {
+								if (pJS.particles.nb == 150) {
+										console.log('mobile mode');
+										pJS.fn.vendors.destroy();
+										pJS_mobile();
+								}
+						}
+				}
 		})
 
 		.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
